39051d357cb2dbc85402387741ad9bf02b9170dd
ayeshabibi-tech <ayeshabibi.tech@gmail.com>
2026-06-05 21:35:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+// Test Cases
+
+console.log(add(2, 3)); // Expected: 5
+console.log(subtract(5, 2)); // Expected: 3
